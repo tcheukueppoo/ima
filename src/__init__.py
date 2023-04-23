8807c3d88223ab0a34378a8403c49c73b761b5d4
tcheukueppo <tcheukueppo@tutanota.com>
2023-04-23 17:41:23 +0000

    __init__.main()

--- src/__init__.py
+++ src/__init__.py
@@ -2,21 +2,22 @@ import sys
 import random
 import re
 
+from math     import inf
 from .image   import Image
 from .search  import Search
 from .options import ParseOptions
 from .        import exceptions
 from .utils   import (
-    get_base_url,
-    # ...
+    humanize_bytes,
+    term_print,
 )
 
 ask = [
-    'High Resolution Images Of {0}',
-    'Images Of {0}',
-    'I Want Images Of {0}',
-    'Download Images Of {0}',
-    'Free Download Images Of {0}',
+    'HIGH RESOLUTION IMAGES OF {0}',
+    'IMAGES OF {0}',
+    'I WANT IMAGES OF {0}',
+    'DOWNLOAD IMAGES OF {0}',
+    'FREE DOWNLOAD IMAGES OF {0}',
 ]
 
 def main():
@@ -25,57 +26,81 @@ def main():
     def _error(error):
         me = sys.argv[0]
         print(me + ': ' + error, file = sys.stderr)
-        exit(1)
 
     def _info(info):
         print(info, file = sys.stdout)
 
     search = Search(engine = opts.engine, save = False)
     if len(args) == 0:
-        _error("No query string, try `{0} --help' for more info".format(me))
+        _error("No query string, try `{0} --help' for more info")
+        exit(1)
 
     for query in args:
-        search.set_engine(query)
+        search.set_engine(ask[1].format(query))
 
         results = []
-        trys, count = 0, 0
-        while count < opts.count:
+        trys, c = 0, 0
+        while True:
             try:
-                while count < opts.count and not opts.search:
-                    if len(result) < opts.result_count:
-                        result += search.next(as_image = True if opts.image_link else False)
-                        result = result[0:opts.result_count]
+                while True:
+                    results = search.next(as_image = False if opts.search else True)
 
-                    if opts.search is not None:
-                        for url in result:
-                            if count == opts.count: break
+                    if opts.search:
+                        for url in results:
+                            c += 1
                             _info(url if len(args) == 1 else '{0},{1}'.format(query, url))
-                            count += 1
-                        continue
+                            if opts.count == c: exit(0)
 
-                    for i in range(0, len(result)):
-                        image = result.pop(i)
+                    for image in results:
+                        image_links = list()
 
-                        _info('[Website] {0}'.format(image.base_url))
-                        if opts.image_link:
-                            hash  = { 'd': 'content', 'l': 'url', 's': 'score' }
-                            links = image.get_links(count = opts.image_count)
+                        if opts.verbose:
+                            _info('[Website] {0}'.format(image.base_url))
 
-                            for link in links:
-                                _info(re.sub(r'(?<!\\)\{(l|s|d)\}', lambda m: link[hash[m.group(1)]], opts.image_link))
-                                count += 1
-                        else:
+                        while True:
                             try:
-                                for stat in image.download(count = opts.image_count):
-                                    if len(stat) != 1: # Header
-                                        _info('[download] filename: ' + stat['filename'] + ', size: ' + utils.humanize_bytes(stat['size']))
-                                        continue
-                                    
+                                cc = c
+                                image_links = []
+                                for link in image.get_links(count = opts.count):
+                                    cc += 1
+                                    image_links.append(link)
+                                    if cc == opts.count: break
+                                break
                             except ConnectionError:
-                                _error('connection error')
+                                _error('[WARN] Failed to connect, trying to reconnect')
+                                if trys == opts.trys: exit(1)
+                                trys += 1
 
-            except exceptions.HTTPResponseError:
-                _error('http response error')
+                            while True:
+                                i= 0
+                                filename = None
+                                try:
+                                    while opts.count != c and (im := range(i, len(image_links))):
+                                        if opts.image_link:
+                                            hash = { 'd': 'content', 'l': 'url', 's': 'score' }
+                                            _info(re.sub(r'(?<!\\)\{(l|s|d)\}', lambda m: image_links[im][hash[m.group(1)]], opts.image_link))
+                                        c += 1
+                                        continue
+                                    for stat in image.download_from(image_links[im]):
+                                        if len(stat) = 1:
+                                            pass
+                                        # Header
+                                        filename = stat['filename']
+                                        _info(
+                                            '[Download] filename: ' +
+                                            stat['filename']        +
+                                            ', size: '              +
+                                            utils.humanize_bytes(stat['size'])
+                                        )
+                                        c += 1
+                                        i += 1
+                                except ConnectionError:
+                                    pass
+                            
+                exit(0)
+            except exceptions.OutOfBoundError:
+                exit(0)
             except ConnectionError:
-                _error('connection error')
-    
+                _error('[WARN] Failed to connect, trying to reconnect') 
+                if trys == opts.trys: exit(1)
+                trys += 1
