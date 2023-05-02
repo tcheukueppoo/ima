19f460655b598523b1ab34e75837aa30a90d5aef
tcheukueppo <tcheukueppo@tutanota.com>
2023-05-02 12:46:41 +0000

    put the forgotten self in class definition

--- tests/test_search.py
+++ tests/test_search.py
@@ -7,20 +7,20 @@ class test_search():
         s = Search()
 
     def test_next_back(self, engine, query):
-        s.set_engine(engine)
-        s.set_query(query)
+        self.set_engine(engine)
+        self.set_query(query)
 
-        r1 = s.next()
-        r2 = s.next()
-        r3 = s.next()
-        b2 = s.back()
-        b1 = s.back()
+        r1 = self.s.next()
+        r2 = self.s.next()
+        r3 = self.s.next()
+        b2 = self.s.back()
+        b1 = self.s.back()
 
         assert r1 == b1
         assert r2 == b2
 
-        f2 = s.next()
-        f3 = s.next()
+        f2 = self.s.next()
+        f3 = self.s.next()
 
         assert f2 == r2
         assert f3 == r3
