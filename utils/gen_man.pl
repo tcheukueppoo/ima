225db0f70654c31c00d41ed996dba192fd2285c5
tcheukueppo <tcheukueppo@tutanota.com>
2023-05-03 14:01:55 +0000

    gen_man can now output synopsis

--- utils/gen_man.pl
+++ utils/gen_man.pl
@@ -6,34 +6,74 @@ use feature qw(say);
 
 use List::Util qw(max);
 
-sub print_options {
-   my $_ = shift
-   my $option;
-   my %docs;
+sub get_options {
+   my $_ = shift;
+   my ( $option, %options );
 
    {
-      if ( m/\G.*?\s*parser[.]add_option(\s*'(?<l-\w)'\s*,\s*'--(?<l>\w*(?:(?>-\w+)+))',\s*/g ) {
-         $option = join ' ', @{$-{l}};
-         redo
+      if ( m/\G.*?\s*parser[.]add_option(\s*'(?<d>-\w)'\s*,\s*'--(?<d>\w*(?:(?>-\w+)+))',\s*/g ) {
+         $option = join ' ', @{$-{d}};
+         redo;
       }
 
-      if ( m/\G.*?(?<h>help\s*=(?:\s*'(?<k>.*?)'\s*)+)/g ) {
-         $doc{$option} = $+{k};
+      if ( m/\G.*?(?<h>help\s*=(?:\s*'(?<h>.+?)'\s*)+)/g ) {
+         $options{$option}{d} = $+{h};
+
          {
-            if ( m/\G\s*'(.+?)'\s*/g ) {
-               join '', $doc{$option} // '', $+{k};
-               redo
+            if ( m/\G\s+'(?<h>.+?)'\s*/g ) {
+               $options{$option}{d} .= $+{h};
+               redo;
             }
          }
-         redo
+         redo;
+      }
+
+      if ( m/\G.*?\s+metavar\s*=\s*'(?<v>.+?)'\s*,/g ) {
+         $options{v} = $+{v};
+         redo;
       }
 
    }
-  
-   my $pad = 5 + max map { length } keys %docs;
-   foreach $opts (keys %docs) {
-      printf '**%s**\n', $opts;
-      say ' ' x $pad, $docs{$opts};
-   }
 
+   return %options;
 }
+
+sub output_options_doc {
+   my $options = shift;
+
+   say '> Mandatory arguments to long options are mandatory for short options too.'
+   foreach $option (keys %options) {
+      printf '> **%s**\n', $opts;
+      printf '>> %s\n', $options{$option};
+   }
+}
+
+sub get_synopsis {
+   my %options = shift
+   my $synopsis = map '[ ' . ( $_ =~ s/ /|/ ) . ( $options{v} // '' =~ s/(.+)/**$1**/ ) . ']', keys %options;
+
+   printf '> **ima** %s **QUERY** [..QUERY]\n', $synopsis
+}
+
+print <<"HEADING"
+# NAME
+
+> ima - Image searcher and downloader
+
+# SYNOPSIS
+
+HEADING
+
+output_synopsis
+output_options_doc
+
+print <<"FOOTER"
+AUTHORS
+> $AUTHORS
+
+REPORTING BUGS
+> $BUGS
+
+COPYRIGHT
+
+FOOTER
